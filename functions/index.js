e7e99cf83a3c9359ea2182c6504416cb8efa408d
BilaArta <bilaarta@gmail.com>
2019-07-26 17:23:53 +0800

    loader

--- functions/index.js
+++ functions/index.js
@@ -3,17 +3,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const firebase = require('firebase-admin')
 const engines = require(`consolidate`);
+const database = require('firebase')
 const path = require('path');
 var nodemailer = require('nodemailer');
 var directTransport = require('nodemailer-direct-transport');
 
+var serviceAccount = require("./iconproject-f66b2-firebase-adminsdk-fddzn-988c643207.json");
+
+
 const cors = require('cors')({origin: true});
 
 require('dotenv').config()
-const mail = express();
+
 const app = express();
-mail.use(cors);
-// app.use(cors);
+app.use(cors);
 
 
 app.use(bodyParser.json());
@@ -29,12 +32,15 @@ const cf = {
     storageBucket: process.env.storageBucket,
     messagingSenderId: process.env.messagingSenderId,
     appId: process.env.appId
-}
+};
 
+const AdminSDK = {
+    credential: firebase.credential.cert(serviceAccount),
+    databaseURL: "https://iconproject-f66b2.firebaseio.com"
+}
 // Initialize Firebase
-firebase.initializeApp(cf);
-const auth = firebase.auth()
-const db = firebase.firestore()
+firebase.initializeApp(AdminSDK)
+const db = firebase.database()
 
 app.engine('hbs', engines.handlebars);
 
@@ -176,7 +182,7 @@ app.get(`*`, (req, res) => {
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 
-function sendEmailApp (){
+function sendEmailApp (sendTo, cc, subject, from){
     // console.log(data.text);
     process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
     // console.log("req.body");
@@ -189,37 +195,72 @@ function sendEmailApp (){
             type: 'login',
             user: process.env.email,
             pass: process.env.pass
-
         }
     });
 
     var mailOptions = {
-        from: 'bilaarta@gmail.com',
-        to: 'bilaartawirawan@gmail.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
+        from: from,
+        to: sendTo,
+        cc: cc,
+        subject: subject,
+        text: 'Test nodemailer To : '+ sendTo +", CC to "+cc
     };
     console.log(".....");
     mail.sendMail(mailOptions, function (error, info) {
         if (error) {
             console.log("error");
-            
             console.log(error);
         } else {
             console.log('Email sent: ' + info.response);
         }
     })
+    return true
 }
+
 exports.app = functions.https.onRequest(app);
 
-exports.test = functions.https.onRequest((req,res) => {
-    console.log(req.body);
-    console.log(req.body.name);
-    console.log(req.data);
-    cors(req,res, () => {
-        console.log(req.body.name);
-        console.log(req.name);
-        console.log(req.body);
-        
+exports.sendmail = functions.https.onRequest((req,res) => {
+    var data = req.body.data
+    var sendTo = data.sendTo.toString()
+    var cc = data.CC.toString()
+    var from = data.from
+    var subject = data.subject
+    // send = sendEmailApp(sendto, cc, data.subject, data.from)
+    // if(send){
+    //     console.log("sent");
+    // }
+
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+    // console.log("req.body");
+    // const text = data.text
+    var mail = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true,
+        auth: {
+            type: 'login',
+            user: process.env.email,
+            pass: process.env.pass
+        }
+    });
+
+    var mailOptions = {
+        from: from,
+        to: sendTo,
+        cc: cc,
+        subject: subject,
+        text: 'Test nodemailer To : '+ sendTo +", CC to "+cc
+    };
+    console.log(".....");
+    return mail.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log("error");
+            console.log(error);
+            res.send({ data: 'Error sent Email' });
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send({ data: 'Email sent' });
+        }
     })
+
 })
